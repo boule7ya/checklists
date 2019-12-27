ef19da874765c3f1ab0426929dca73ff65eff6f5
Mohamed <boule7ya@hotmail.fr>
2019-12-27 15:59:16 +0100

    init

---
+++ Checklists/Data Models/Checklist.swift
@@ -0,0 +1,29 @@
+//
+//  Checklist.swift
+//  Checklists
+//
+//  Created by Anypli on 13/11/2019.
+//  Copyright © 2019 Anypli. All rights reserved.
+//
+
+import UIKit
+
+class Checklist: NSObject, Codable {
+    var name = ""
+    var items = [ChecklistItem]()
+    var iconName = "No Icon"
+    init(name: String, iconName: String = "No Icon") {
+        self.name = name
+        self.iconName = iconName
+        super.init()
+    }
+    func countUncheckedItems() -> Int {
+        var count = 0
+        for item in items where !item.checked {
+            count += 1
+        }
+        return count
+    }
+    
+    
+}
